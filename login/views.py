5c3d8b2421c5113551e7061594c7c42507f80a05
root <root@localhost.(none)>
2015-10-27 15:20:25 +0800

    fix next page bug

--- login/views.py
+++ login/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.utils.log import logger
 from django.contrib import auth
+from Queue import Queue
 from django.contrib.auth.decorators import login_required
 
 
@@ -15,11 +16,19 @@ def login_auth(request):
     authed = auth.authenticate(username=user_auth,password=passwd_auth)
     if authed and authed.is_active:
         auth.login(request,authed)
-        if globals().has_key('next_next') and not next_next == None:
-            logger.info('<%s> login in sucess.' % user_auth)
-            return HttpResponseRedirect(next_next)
+        # if globals().has_key('next_next') and not next_next == None:
+        #     logger.info('<%s> login in sucess.' % user_auth)
+        #     next_page = next_next
+        #     globals().pop('next_next')
+        #     return HttpResponseRedirect(next_page)
+        # else:
+        #     logger.info('<%s> login in sucess.' % user_auth)
+        #     return HttpResponseRedirect('/main/')
+        next_page = request.session.get('next')
+        if next_page:
+            request.session.pop('next')
+            return HttpResponseRedirect(next_page)
         else:
-            logger.info('<%s> login in sucess.' % user_auth)
             return HttpResponseRedirect('/main/')
     else:
         logger.warn('<%s> login in fail.' % user_auth)
@@ -31,6 +40,7 @@ def logout(request):
 
 def not_login(request):
     next_next = request.GET.get('next')
-    print next_next
-    global next_next
+    # print next_next
+    # global next_next
+    request.session['next'] = next_next
     return render(request,'login/login.html',{'msg':u'您还没有登录'})
